844a01559f2b936071f871fcf2585ac3a03b853f
Francisco <jscriptcoder@gmail.com>
2023-01-08 20:17:01 +0100

    Configure Alchemy Goerli testnet

--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@ require('dotenv').config()
 
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-const { MNEMONIC, PROJECT_ID } = process.env
+const { MNEMONIC, PROJECT_URL } = process.env
 
 module.exports = {
   /**
@@ -84,13 +84,13 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, PROJECT_URL),
+      network_id: 5, // Goerli's id
+      confirmations: 2, // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
