a298eadac1d398132104813959c198070f6f7f7a
Francisco <jscriptcoder@gmail.com>
2023-01-23 11:31:34 +0100

    Minor change

--- migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-var Lottery = artifacts.require('Lottery')
-var migration = function (deployer) {
-  deployer.deploy(Lottery)
-}
-module.exports = migration
